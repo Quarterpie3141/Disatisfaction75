109c1704c6f1d46a724576909ee0f955160a6b74
quaterpie3141 <86412584+Quarterpie3141@users.noreply.github.com>
2022-03-22 21:11:32 +0800

    Added header and cpp files

---
+++ Software/Disatisfaction.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup()
+{
+
+pinMode(PB7, INPUT);
+pinMode(PE6, OUTPUT);
+
+};
+
+void loop()
+{
+
+
+
+};
---
+++ Software/Disatisfaction.h
@@ -0,0 +1,22 @@
+/* 
+disatisfaction 75 code 
+made by Quraterpie3141
+@Quarterpie3141
+*/
+
+
+#ifndef Disatisfaction_h
+#define Disatisfaction_h
+
+#include "Arduino.h"
+
+class Disatisfaction
+{
+
+public:
+
+private:
+
+};
+
+#endif
