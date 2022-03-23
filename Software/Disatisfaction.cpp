37c127cc9e9f26c66a515fe42d7d208498ad446d
quaterpie3141 <86412584+Quarterpie3141@users.noreply.github.com>
2022-03-23 21:42:14 +0800

    added some loops

--- Software/Disatisfaction.cpp
+++ Software/Disatisfaction.cpp
@@ -1,11 +1,38 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <Keypad.h>
+
+ 
+
+
+
+
+// KEYMAP/ keyboard layout 1 in ascii codes
+  const byte ROWS = 6;
+  const byte COLS = 14;
+
+  char keys[ROWS][COLS]={
+
+{'','2','3','4','','','','','','','','','','',},
+{'','','','','','','','','','','','','','',},
+{'','','','','','','','','','','','','','',},
+{'','','','','','','','','','','','','','',},
+{'','','','','','','','','','','','','','',},
+{'','','','','','','','','','','','','','',}
+ 
+};
+//pin defenitions for the keypad libary 
+byte rowPins[ROWS] = {PF0, PF1, PF4, PF5, PF6, PF7}; 
+byte colPins[COLS] = {PC7, PC6, PD7, PD6, PD5, PD4, PD3, PD2, PB7, PB6, PB5, PB4, PB3, PB2}; 
+
+Keypad Disatisfaction = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); // if you wan tto change the keymap then create a new array and replace makeKeymaps(keys) with the name of the array
+
+
 
 
 void setup() {
   
-  
-  /* pin defentions for the rows and coloums 
+   /* pin defentions for the rows and coloums 
   of the keyboard matrix. The current flows from 
   the coloums and is read by each of the rows sequentially
   */
@@ -35,28 +62,34 @@ void setup() {
   pinMode(PF6,INPUT);
   pinMode(PF7,INPUT);
 
-Serial.begin(9600);
+
+
+// setting the debounce and repeat times(milliseconds)
+Disatisfaction.setHoldTime(800);// time untill the keypress is repeated
+Disatisfaction.setDebounceTime(10);// time untill new keypresses are accepted
+
+
+}
+
+void loop()
+{
+
+
+
 
 
 
 }
 
 
-
-
-void Matrix() {
+void scanMatrix() {
+ char key = Disatisfaction.getKey();
  
-
-
-
-
-
+ if(key)
 
 
 
 }
-
-
 
 void OLED(){
 
@@ -66,6 +99,10 @@ void OLED(){
 
 }
 
-
-
 void QTENCODER(){
+
+
+
+
+
+}
