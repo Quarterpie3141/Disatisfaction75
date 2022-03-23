1043d34894e448cc6f5eea1160c6e0202ce4d334
quaterpie3141 <86412584+Quarterpie3141@users.noreply.github.com>
2022-03-23 14:58:47 +0800

    Update Disatisfaction.cpp

--- Software/Disatisfaction.cpp
+++ Software/Disatisfaction.cpp
@@ -1,30 +1,71 @@
-#include "Arduino.h"
-#include "Disatisfaction.h"
-
-void setup()
-{
-
-pinMode(PB7, INPUT);
-
-pinMode(PC7, OUTPUT);
-pinMode(PC6, OUTPUT);
-pinMode(PD7, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-pinMode(, OUTPUT);
-};
-
-void loop()
-{
+#include <Arduino.h>
+#include <Wire.h>
 
 
+void setup() {
+  
+  
+  /* pin defentions for the rows and coloums 
+  of the keyboard matrix. The current flows from 
+  the coloums and is read by each of the rows sequentially
+  */
+  
+  
+  //Col pins
+  pinMode(PC7,OUTPUT);
+  pinMode(PC6,OUTPUT);
+  pinMode(PD7,OUTPUT);
+  pinMode(PD6,OUTPUT);
+  pinMode(PD5,OUTPUT);
+  pinMode(PD4,OUTPUT);
+  pinMode(PD3,OUTPUT);
+  pinMode(PD2,OUTPUT);
+  pinMode(PB7,OUTPUT);
+  pinMode(PB6,OUTPUT);
+  pinMode(PB5,OUTPUT);
+  pinMode(PB4,OUTPUT);
+  pinMode(PB3,OUTPUT);
+  pinMode(PB2,OUTPUT);
 
-};
+  //Row pins
+  pinMode(PF0,INPUT);
+  pinMode(PF1,INPUT);
+  pinMode(PF4,INPUT);
+  pinMode(PF5,INPUT);
+  pinMode(PF6,INPUT);
+  pinMode(PF7,INPUT);
+
+Serial.begin(9600);
+
+
+
+}
+
+
+
+
+void Matrix() {
+ 
+
+
+
+
+
+
+
+
+}
+
+
+
+void OLED(){
+
+
+
+
+
+}
+
+
+
+void QTENCODER(){
